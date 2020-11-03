3177db340a9bb631ec5783acb63944220b8369e7
elietmsoft <elietshibangu347@gmail.com>
2020-11-03 14:52:44 +0100

    fin game for biginner 01

---
+++ game1/app.js
@@ -0,0 +1,51 @@
+const config = {
+    width:500,
+    height:300,
+    type:Phaser.AUTO,
+    physics:{
+        default: 'arcade',
+        arcade:{
+            gravity:{y: 450}
+        }
+
+    },
+    scene:{
+        preload:preload,
+        create:create,
+        update:update
+    }
+}
+
+var game = new Phaser.Game(config);
+let logo;
+let cursors;
+
+function preload() {
+    this.load.image('logo','/images/logo.PNG');
+}
+function create() {
+   logo= this.physics.add.image(100,100,'logo');
+   logo.body.collideWorldBounds = true;
+   cursors = this.input.keyboard.createCursorKeys();
+
+    
+}
+function update() { 
+    //permet de ne plus faire déplacer notre image(logo)
+    logo.setVelocityX(0);
+    if(cursors.up.isDown)
+    {
+        //si qlq1 à appuyer sur une touche d'en haut
+        logo.setVelocity(0,-300);
+    }
+    if(cursors.right.isDown)
+    {
+        //si qlq1 à appuyer sur une touche à droite
+        logo.setVelocity(300,0);
+    }
+    if(cursors.left.isDown)
+    {
+        //si qlq1 à appuyer sur une touche à gauche
+        logo.setVelocity(-300,0);
+    }
+}
